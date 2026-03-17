f22baff225df7a12e967210f98e1a8c9b8adee03
Irina Karamyan <I22.Karamyan.Irina@etud.ufar.am>
2026-03-17 18:51:04 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
-print("backend started")
+# Simple fraud detection logic
+
+def detect_fraud(transaction):
+    amount = transaction["amount"]
+    location = transaction["location"]
+
+    # simple rules
+    if amount > 10000:
+        return "High Risk"
+
+    if location == "Unknown":
+        return "Medium Risk"
+
+    return "Low Risk"
+
+
+# test data
+transaction1 = {"amount": 15000, "location": "Yerevan"}
+transaction2 = {"amount": 200, "location": "Unknown"}
+transaction3 = {"amount": 100, "location": "Yerevan"}
+
+print(detect_fraud(transaction1))
+print(detect_fraud(transaction2))
+print(detect_fraud(transaction3))
